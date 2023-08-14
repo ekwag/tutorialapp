4aa7ed8107930ec95f11555f95f9504322037e8b
Ayush Gupta <136375040+testingpurposesite@users.noreply.github.com>
2023-08-14 20:42:31 +0530

    commit

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d2dc84638a131699ab3c3634e0d9d34ce79c6a91679817b5c4824db519424487
+size 1336
---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:50b51dfb1d4fa76e0c1445ccce227fd35f8f80dfcdeab982320763a96dfe0c3d
+size 11381
